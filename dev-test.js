bc55ca7d24c9714cdf93074fb93975584537bb65
agnelcomp2020 <49745710+agnelcomp2020@users.noreply.github.com>
2019-04-19 20:51:11 +0530

    Delete dev-test.js

--- dev-test.js
+++
@@ -1,9 +0,0 @@
-const Block = require('./block');
-
-/*const block = new Block('foo','bar','zoo','haz');
-console.log(block.toString());
-console.log(Block.genesis().toString());
-console.log(Block.mineBlock().toString());*/
-
-const fooBlock=Block.mineBlock(Block.genesis(),'foo');
-console.log(fooBlock.toString());
