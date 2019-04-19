6be37c3f73e95dcb03102b8c3d2e6fd5d5da7216
agnelcomp2020 <49745710+agnelcomp2020@users.noreply.github.com>
2019-04-19 20:05:47 +0530

    Update svm.py

--- group 22/prediction/svm.py
+++ group 22/prediction/svm.py
@@ -1,5 +1,5 @@
 from sklearn import svm
 clf = svm.SVC()
-clf.fit(X_train2, y_train)  
-y_pred = clf.predict(X_test)
+clf.fit(X_train, y_train)  
+y_pred = clf.predict(Test_data)
 printScores(y_test, y_pred, "svm")
